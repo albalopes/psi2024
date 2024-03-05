18aad5e0f3bcf7e92ec618a7f094518b294c0efc
albalopes <alba.lopes@ifrn.edu.br>
2024-03-05 14:41:39 +0000

    Passando parâmetros para o HTML e nas rotas

--- app.py
+++ app.py
@@ -9,9 +9,29 @@ def index():
 @app.route('/compras')
 def compras():
     #return '<ul><li>Arroz</li></ul>'
-    return render_template('compras.html')
+    return render_template('compras.html', item1 = 'Farinha', item2='Cuscuz')
 
 @app.route('/mercados')
 def mercados():
     return render_template('mercados.html')
 
+'''
+@app.route('/gastos')
+def gastos():
+    mes = 'Fevereiro'
+    valor = '843,00'
+    return render_template('gastos.html', a = mes, b = valor)
+'''
+
+@app.route('/gastos', defaults={"mes": "Janeiro", "valor": "0"})
+@app.route('/gastos/<mes>/<valor>')
+def gastos(mes, valor):
+    return render_template('gastos.html', a = mes, b = valor)
+
+
+@app.route('/dobro', defaults={"n":0})
+@app.route('/dobro/<int:n>')
+@app.route('/dobro/<float:n>')
+def dobro(n):
+    resultado = 2*n
+    return render_template('dobro.html', n=n, resultado=resultado)
